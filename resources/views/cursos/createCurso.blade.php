5f5ee694ba66c0396177bbce7b6c40d83c8a5de9
LuisEduardo <ketchsoul@gmail.com>
2018-06-03 15:23:50 -0400

    actualizaaaaaacion

---
+++ resources/views/cursos/createCurso.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+@section('content')
+
+<body>
+<h1 align="center">Detalles del Proyecto</h1>
+    <div class="form-group row">
+			<label for="buscador" class="col-sm-3 col-form-label col-form-label-lg">Nombre Proyecto :</label>
+    </div>
+    <div class="form-group row">
+			<label for="buscador" class="col-sm-3 col-form-label col-form-label-lg">Areas Proyecto :</label>
+    </div>
+    <div class="form-group row">
+			<label for="buscador" class="col-sm-3 col-form-label col-form-label-lg">Sub Areas Proyecto :</label>
+    </div>
+    <div class="form-group row">
+			<label for="buscador" class="col-sm-3 col-form-label col-form-label-lg">Estudiantes :</label>
+    </div>
+    <div class="form-group row">
+			<label for="buscador" class="col-sm-3 col-form-label col-form-label-lg">Tutor :</label>
+    </div>
+    <div class="form-group row">
+			<label for="buscador" class="col-sm-3 col-form-label col-form-label-lg">Tribunales Asignados :</label>
+    </div>
+    <div class="form-group row">
+			<label for="buscador" class="col-sm-3 col-form-label col-form-label-lg">Estado Proyecto :</label>
+    </div>
+</body>
+
+@endsection
