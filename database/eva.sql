14a4413b42c2c7c292bcdaebbf23b003b4845544
kenny <kennydaltonc@yahoo.com>
2018-06-10 20:30:17 -0400

    base de datos

--- database/eva.sql
+++ database/eva.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 29, 2018 at 07:51 PM
+-- Generation Time: Jun 11, 2018 at 02:28 AM
 -- Server version: 10.1.9-MariaDB
 -- PHP Version: 5.6.15
 
@@ -23,35 +23,144 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `carrera`
+--
+
+CREATE TABLE `carrera` (
+  `idCarrera` int(11) NOT NULL,
+  `nombreCarrera` varchar(60) NOT NULL,
+  `users_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `curso`
+--
+
+CREATE TABLE `curso` (
+  `idCurso` int(11) NOT NULL,
+  `nombreCurso` varchar(300) NOT NULL,
+  `objetivos` text NOT NULL,
+  `descripcion` text NOT NULL,
+  `codigoCurso` varchar(45) DEFAULT NULL,
+  `fechaCreacion` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `curso`
+--
+
+INSERT INTO `curso` (`idCurso`, `nombreCurso`, `objetivos`, `descripcion`, `codigoCurso`, `fechaCreacion`) VALUES
+(3303, 'Introduccion a la Programacion', 'objetivo 1\r\nobjetivo 2\r\nobjetivo 3\r\nobjetivo 4\r\nobjetivo 5\r\nobjetivo 6', 'Materia troncal de las carreras informatica Sistemas', 'Intro', '2018-06-10'),
+(3304, 'Elementos de Programacion', 'objetivo 1\r\nobjetivo 2\r\nobjetivo 3\r\nobjetivo 4\r\nobjetivo 5\r\nobjetivo 6', 'Materia troncal de las carreras informatica Sistemas', 'Elementos', '2018-06-10'),
+(3305, 'Taller de Programacion', 'Objetivo 1\r\nObjetivo 2\r\nObjetivo 3\r\nObjetivo 4\r\n', 'Aplicacion de conocimientos', 'TallerProgra', '2018-06-10'),
+(3306, 'Calculo 1', 'Objetivo 1\r\nObjetivo 2\r\nObjetivo 3\r\nObjetivo 4\r\nObjetivo 5', 'Calculos de integrales y derivadas', 'Calculo1', '2018-06-10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `curso_usuario`
+--
+
+CREATE TABLE `curso_usuario` (
+  `curso_idCurso` int(11) NOT NULL,
+  `users_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tareas`
+--
+
+CREATE TABLE `tareas` (
+  `idTareas` int(11) NOT NULL,
+  `fechaInicio` date NOT NULL,
+  `fechaFin` date NOT NULL,
+  `descripcion` text,
+  `tema_idTema` int(11) NOT NULL,
+  `fechaEntrega` text,
+  `documento` text,
+  `estadoEntrega` enum('No entregada','Entregada') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tema`
+--
+
+CREATE TABLE `tema` (
+  `idTema` int(11) NOT NULL,
+  `nombreTema` varchar(60) NOT NULL,
+  `descripcionTema` text NOT NULL,
+  `contenido` text,
+  `curso_idCurso` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `name` varchar(70) NOT NULL,
+  `name` varchar(100) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  `remember_token` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
-  `lastname` varchar(100) NOT NULL,
-  `tipo` enum('estudiante','docente','','') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `lastname` varchar(100) DEFAULT NULL,
+  `tipo` enum('estudiante','docente') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `updated_at`, `remember_token`, `lastname`, `tipo`) VALUES
-(1, 'kenny', 'kennydaltonc@yahoo.com', '$2y$10$kqLoKKKA1tVaXN8JEMDfiuB0uaO6A6i8F0UWvq7NqUHVNUgfkPzuW', '2018-05-28 20:37:25', '2018-05-29 21:47:37', 'CEtvWyiarpe1xG79AiV7xJjWDPmkQNN9AW6ZPkx8BROWnfT5tVgXTT72N8yW', '', 'estudiante'),
-(2, 'pedro', 'pedro@yahoo.com', '$2y$10$Vv35eN8X4tEWZPloYH3Z0uSfmF/j7YGnNc25S8KNrANKCe6KHL09q', '2018-05-29 05:23:48', '2018-05-29 21:48:02', 'hs9T3ds3sdxFQwtpiX4hStQLvxiFlwS6JkFcvm2nYoktyhfxQRXajQ66WatY', '', 'estudiante'),
-(4, 'adc', 'adca@ad.com', '$2y$10$dJsucdzTJe22UrQglkI/8.czrAF4WkxMUycRp2NpsW.vesTjWAhoy', '2018-05-29 20:59:20', '2018-05-29 20:59:58', 'XAk3ANIxYpuIzNRRwSxm3t3vDp9HXmwqrDguVTFSjqPO361iz1gxvv7OlG6J', 'adc', 'estudiante'),
-(5, 'asdcas', 'asdc@hasdc.com', '$2y$10$1RAfNT05AOeEItcb5nPMbODrDOVXbGnv4x2BIO/r2aKy/Q.xMHjo2', '2018-05-29 21:00:35', '2018-05-29 21:02:04', 'mn7b1PZ4YVmvcBLEOUZkOhxVPFO1Fj1PrN5w3OHGzeBApUMJSmliI8h2wLC9', 'csdacasdc', 'docente'),
-(6, 'asda', 'acadsdcas@dscas.com', '$2y$10$XpOJ6MQ8dwxUhMcGw.tJcOlSajqNK6lKVDY3C/gvfZvfvQeacLyMG', '2018-05-29 21:48:28', '2018-05-29 21:48:35', '1jYMU2drc4wKcyt78GHqVYQ1Nb9RjTKYwwkYe6b3DkcO6V525CIduqImnuiv', 'cdascas', 'docente');
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`, `lastname`, `tipo`) VALUES
+(6, 'Kenny', 'kennydaltonc@yahoo.com', '$2y$10$RotW6m9K3OeBO5AFTPgEb.0aoOFnkPPVSXJvoUfPSG7bJ/oveGvf6', NULL, '2018-06-11 04:05:11', '2018-06-11 04:05:11', 'Dalton', 'docente');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `carrera`
+--
+ALTER TABLE `carrera`
+  ADD PRIMARY KEY (`idCarrera`),
+  ADD KEY `fk_carrera_users1_idx` (`users_id`);
+
+--
+-- Indexes for table `curso`
+--
+ALTER TABLE `curso`
+  ADD PRIMARY KEY (`idCurso`);
+
+--
+-- Indexes for table `curso_usuario`
+--
+ALTER TABLE `curso_usuario`
+  ADD KEY `fk_curso_usuario_curso1_idx` (`curso_idCurso`),
+  ADD KEY `fk_curso_usuario_users1_idx` (`users_id`);
+
+--
+-- Indexes for table `tareas`
+--
+ALTER TABLE `tareas`
+  ADD PRIMARY KEY (`idTareas`),
+  ADD KEY `fk_tareas_tema1_idx` (`tema_idTema`);
+
+--
+-- Indexes for table `tema`
+--
+ALTER TABLE `tema`
+  ADD PRIMARY KEY (`idTema`),
+  ADD KEY `fk_tema_curso1_idx` (`curso_idCurso`);
 
 --
 -- Indexes for table `users`
@@ -64,10 +173,54 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `carrera`
+--
+ALTER TABLE `carrera`
+  MODIFY `idCarrera` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `curso`
+--
+ALTER TABLE `curso`
+  MODIFY `idCurso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3307;
+--
+-- AUTO_INCREMENT for table `tareas`
+--
+ALTER TABLE `tareas`
+  MODIFY `idTareas` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `carrera`
+--
+ALTER TABLE `carrera`
+  ADD CONSTRAINT `fk_carrera_users1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `curso_usuario`
+--
+ALTER TABLE `curso_usuario`
+  ADD CONSTRAINT `fk_curso_usuario_curso1` FOREIGN KEY (`curso_idCurso`) REFERENCES `curso` (`idCurso`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_curso_usuario_users1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `tareas`
+--
+ALTER TABLE `tareas`
+  ADD CONSTRAINT `fk_tareas_tema1` FOREIGN KEY (`tema_idTema`) REFERENCES `tema` (`idTema`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `tema`
+--
+ALTER TABLE `tema`
+  ADD CONSTRAINT `fk_tema_curso1` FOREIGN KEY (`curso_idCurso`) REFERENCES `curso` (`idCurso`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
